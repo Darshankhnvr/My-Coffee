4b5848c636f7e3f2cd25ed2a9a069f896262ecca
Darshankhnvr <darshankgoudahnvr@gmail.com>
2025-04-13 18:00:58 +0530

    final

---
+++ constants/MenuImages.js
@@ -0,0 +1,16 @@
+import pic2 from "@/assets/images/menu/pic2.jpeg";
+import pic3 from "@/assets/images/menu/pic3.jpeg";
+import pic4 from "@/assets/images/menu/pic4.jpeg";
+import pic5 from "@/assets/images/menu/pic5.jpeg";
+import pic6 from "@/assets/images/menu/pic6.jpeg";
+import pic7 from "@/assets/images/menu/pic7.jpeg";
+
+export default [
+    
+    pic2,
+    pic3,
+    pic4,
+    pic5,
+    pic6,
+    pic7
+]
---
+++ constants/MenuItems.js
@@ -0,0 +1,33 @@
+export const MENU_ITEMS = [
+    {
+        "id":1,
+        "title":"Espresso",
+        "desc": "Strong, concentrated coffee"
+    },
+    {
+        "id":2,
+        "title":"Latte",
+        "desc": "Warm milk with espresso"
+    },
+    {
+        "id":3,
+        "title":"Cappuccino",
+        "desc": "Espresso, hot milk and steamed milk foam"
+    },
+    {
+        "id":4,
+        "title":"Americano",
+        "desc": "Espresso with hot water"
+    },
+    {
+        "id":5,
+        "title":"Mocha",
+        "desc": "Espresso, hot milk and steamed milk foam"
+    },
+    {
+        "id":6,
+        "title":"cold",
+        "desc": "Espresso, hot milk and steamed milk foam"
+    }
+
+]
